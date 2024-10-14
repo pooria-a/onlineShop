c5e090e8dc080b4da4494dfe9900f39ba8dfe459
Pooria <pooriaabdollahi@email.com>
2024-10-14 13:49:10 +0330

    Brand and type added as entity

---
+++ Core/Interfaces/IProductRepository.cs
@@ -0,0 +1,16 @@
+using Core.Entities;
+using System.Threading.Tasks; 
+using System.Collections.Generic;
+
+
+namespace Core.Interfaces
+{
+ 
+    public interface IProductRepository{
+
+        Task<Product> GetProductByIdAsync(int id);
+        Task<IReadOnlyList<Product>> GetProductsAsync();
+        Task<IReadOnlyList<ProductType>> GetProductsTypesAsync();
+        Task<IReadOnlyList<ProductBrand>> GetProductsBrandsAsync();
+    }
+}
