bb3b0658ea45149afaed645afb833b1d2b1547d4
Pooria <pooriaabdollahi@email.com>
2024-10-22 10:03:55 +0330

    clean program.cs

---
+++ API/Errors/ApiValidationErrorResponse.cs
@@ -0,0 +1,12 @@
+using System.Security.Cryptography.X509Certificates;
+
+namespace API.Errors
+{
+    public class ApiValidationErrorResponse : ApiResponse
+    {
+        public ApiValidationErrorResponse() : base(400)
+        {
+        }
+            public IEnumerable<string> Errors {get; set;}
+    }
+}
